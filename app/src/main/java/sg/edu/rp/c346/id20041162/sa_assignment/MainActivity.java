f9ee45a7162681fdc6ecce62fbc50dd112a3975f
SeanAwh <20041162@myrp.edu.sg>
2021-07-06 06:53:30 +0800

    v2

--- app/src/main/java/sg/edu/rp/c346/id20041162/sa_assignment/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20041162/sa_assignment/MainActivity.java
@@ -1,14 +1,17 @@
-package sg.edu.rp.c346.id20041162.sa_assignment;
+ package sg.edu.rp.c346.id20041162.sa_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
     Button listAddBtn;
+    TextView tvLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listAddBtn = findViewById(R.id.listAddBtn);
+        tvLink = findViewById(R.id.tvLink);
+
+        tvLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/SeanAwh/SA_Assignment.git"));
+                startActivity(intentWeb);
+            }
+        });
+
 
         listAddBtn.setOnClickListener(new View.OnClickListener() {
             @Override
